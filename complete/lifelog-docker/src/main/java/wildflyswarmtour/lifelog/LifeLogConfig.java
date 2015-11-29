53d3d4d2fe974b1256c4ec23381cd33f505b5cfc
emag <lanabe.lanabe@gmail.com>
2015-11-29 21:38:43 +0900

    Enable link for keycloak

--- complete/lifelog-docker/src/main/java/wildflyswarmtour/lifelog/LifeLogConfig.java
+++ complete/lifelog-docker/src/main/java/wildflyswarmtour/lifelog/LifeLogConfig.java
@@ -1,5 +1,12 @@
 package wildflyswarmtour.lifelog;
 
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * @author Yoshimasa Tanabe
  */
@@ -44,6 +51,52 @@ class LifeLogConfig {
     }
 
     return dbPort;
+  }
+
+  static String kyeCloakJsonFromTemplate() {
+    ClassLoaderAsset keycloakJsonTemplate = new ClassLoaderAsset("keycloak.json.template", App.class.getClassLoader());
+
+    try (InputStream in = keycloakJsonTemplate.openStream()) {
+      JsonObject template = Json.createReader(in).readObject();
+      return Json.createObjectBuilder()
+        .add("realm", template.getString("realm"))
+        .add("realm-public-key", template.getString("realm-public-key"))
+        .add("bearer-only", template.getBoolean("bearer-only"))
+        .add("auth-server-url", String.format("http://%s:%d/auth", keycloakHost(), keycloakPort()))
+        .add("ssl-required", template.getString("ssl-required"))
+        .add("resource", template.getString("resource"))
+        .build().toString();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    throw new RuntimeException("Something wrong with keycloak.json.template.");
+  }
+
+  static private String keycloakHost() {
+    String keycloakHost = "localhost";
+
+    if (System.getenv("KEYCLOAK_PORT_8080_TCP_ADDR") != null) {
+      keycloakHost = System.getenv("KEYCLOAK_PORT_8080_TCP_ADDR");
+    }
+    if (System.getProperty("lifelog.keycloak.host") != null) {
+      keycloakHost = System.getProperty("lifelog.keycloak.host");
+    }
+
+    return keycloakHost;
+  }
+
+  static private int keycloakPort() {
+    int keycloakPort = 8180;
+
+    if (System.getenv("KEYCLOAK_PORT_8080_TCP_PORT") != null) {
+      keycloakPort = Integer.parseInt(System.getenv("KEYCLOAK_PORT_8080_TCP_PORT"));
+    }
+    if (System.getProperty("lifelog.db.port") != null) {
+      keycloakPort = Integer.parseInt(System.getProperty("lifelog.db.port"));
+    }
+
+    return keycloakPort;
   }
 
 }
--- complete/lifelog-docker/src/main/java/wildflyswarmtour/lifelog/LifeLogDeployment.java
+++ complete/lifelog-docker/src/main/java/wildflyswarmtour/lifelog/LifeLogDeployment.java
@@ -2,6 +2,7 @@ package wildflyswarmtour.lifelog;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
 import org.wildfly.swarm.keycloak.Secured;
 
@@ -17,6 +18,9 @@ public class LifeLogDeployment {
 
     deployment.addAsWebInfResource(
       new ClassLoaderAsset("META-INF/persistence.xml", App.class.getClassLoader()), "classes/META-INF/persistence.xml");
+
+    deployment.addAsWebInfResource(
+      new StringAsset(LifeLogConfig.kyeCloakJsonFromTemplate()), "keycloak.json");
 
     deployment.as(Secured.class)
       .protect("/entries/*")
